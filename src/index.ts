4a46b5f09c3b7b005198bd1f2899b1dc3983d07c
LandonHarter <landonharter@outlook.com>
2025-08-19 23:46:05 -0400

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,39 @@ import { serve } from "@hono/node-server";
 import chat from "./chat.js";
 import { getProviderKeys, isValidApiKey } from "./lib/keys.js";
 import { createHono } from "./hono.js";
+import { cors } from "hono/cors";
 
 const app = createHono().basePath("/v1");
+app.use(
+	"*",
+	cors({
+		origin: "*",
+		allowHeaders: ["Authorization", "Content-Type"],
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	})
+);
+
 app.use("*", async (c, next) => {
 	const auth = c.req.header("Authorization");
 	if (!auth) {
+		console.log("No auth header found");
 		return c.json({ error: "Unauthorized" }, 401);
 	}
 
 	const key = auth.split(" ")[1];
 	if (!key) {
+		console.log("No key found");
 		return c.json({ error: "Unauthorized" }, 401);
 	}
 	const isValid = await isValidApiKey(key);
 	if (!isValid) {
+		console.log("Invalid key");
 		return c.json({ error: "Unauthorized" }, 401);
 	}
 
 	const providerKeys = await getProviderKeys(key);
 	if (!providerKeys) {
+		console.log("No provider keys found");
 		return c.json({ error: "Unauthorized" }, 401);
 	}
 
